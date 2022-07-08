3b0ec83a922de6008cb067e7852d3d535b53cfc4
Kaisar Yerdenbekov <sysreqx@gmail.com>
2022-07-08 12:42:38 +0600

    Init

---
+++ src/main/java/kz/kaisar/spring_hibernate_app_01/dao/PersonDAO.java
@@ -0,0 +1,48 @@
+package kz.kaisar.spring_hibernate_app_01.dao;
+
+import kz.kaisar.spring_hibernate_app_01.models.Person;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public PersonDAO(SessionFactory sessionFactory) {
+
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Transactional
+    public List<Person> index() {
+        Session session = sessionFactory.getCurrentSession();
+
+        List<Person> people = session.createQuery("select p from Person p", Person.class)
+                .getResultList();
+
+        return people;
+    }
+
+    public Person show(int id) {
+        return null;
+    }
+
+    public void save(Person person) {
+
+    }
+
+    public void update(int id, Person updatedPerson) {
+
+    }
+
+    public void delete(int id) {
+
+    }
+}
